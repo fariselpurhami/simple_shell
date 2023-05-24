f305300d4e7b6f0d097f25e81cfec92eac32cf72
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-05-24 09:51:48 +0300

    shell.h

--- shell.h
+++ shell.h
@@ -21,19 +21,5 @@ extern char **environ;
 char **split_command(char *line);
 void splitSpace(char *argv[], char line2[]);
 void split(char line2[], char *slicedCommand[], char c[]);
-int execute(char line2[], int cont, char *v, char *t, char *n);
-int count(char line2[], char c);
-int salir(char *line, char *line2, int cont, char **com, char *name, int e);
-char *readc(void);
-char *_cpy(char *dest, char *src);
-char *gpath();
-char **splitPath(char *path);
-char *checkPath(char **dpath, char *argv0);
-char *unePath(char *dpath, char *argv0);
-char *_strchr(char *s, char c);
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-char *_itoa(int n);
 
 #endif /* SHELL_H */
